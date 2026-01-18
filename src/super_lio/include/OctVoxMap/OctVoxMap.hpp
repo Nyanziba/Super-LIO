9dbde5e5c6976198f5dca9698f6f082da0f78ca1
Liansheng Wang <lswang@mail.ecust.edu.cn>
2026-01-18 13:33:55 +0800

    opt map update logic

--- src/super_lio/include/OctVoxMap/OctVoxMap.hpp
+++ src/super_lio/include/OctVoxMap/OctVoxMap.hpp
@@ -298,8 +298,7 @@ void OctVoxMap<Point, Scalar>::insert(const Points& cloud_world){
     } else {
       iter->second->second.AddPoint(pt, local_idx);
       data_.splice(data_.begin(), data_, iter->second);
-      grids_.erase(iter);
-      grids_.insert(std::make_pair(key, data_.begin()));
+      iter.value() = data_.begin();
     }
   }
 }
